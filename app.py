fb06af4ecb5368027950fc0de3bcae70199cedc9
titnogueira <97749893+titnogueira@users.noreply.github.com>
2022-01-14 17:26:58 +0100

    final update i hope

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, from inside the updated application!"
+    return "Hello World, from inside the updated again the application!"
 
 
 if __name__ == "__main__":
